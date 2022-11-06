7fd8f87a2a15e2b132bc44d4e27c10531ab7ba45
dblatcher <dv.bltchr@gmail.com>
2022-11-06 14:51:38 +0000

    reset component

--- src/component.tsx
+++ src/component.tsx
@@ -1,5 +1,4 @@
 import { h, VNode } from 'preact';
-import UIForHead from './components/UIForHead';
 
 import './style.css';
 
@@ -11,7 +10,6 @@ export default function App(props: Props): VNode {
     return (
         <div>
             <h1 style={{ color: props.color }}>Hello, World!</h1>
-            <UIForHead />
         </div>
     );
 }
